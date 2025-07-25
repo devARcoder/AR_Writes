7ae03b6b76053fe4303974d40d3dc595cf70c2d3
Abdur Razzaq <devarcoder789@gmail.com>
2025-07-25 18:55:12 +0500

    code clean up

--- src/components/headers/Header.jsx
+++ src/components/headers/Header.jsx
@@ -1,24 +1,20 @@
 import { Bell, Menu, Search, SquarePen, X } from "lucide-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Input from "../shared/common/Input";
-import SearchBar from "./SearchBar";
 import Sidebar from "./Sidebar";
-import Button from "../shared/common/Button";
 import Navbar from "./Navbar";
 
 const Header = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isLogged, setIsLogged] = useState(false); // Manage user login state
+  const [isLogged, setIsLogged] = useState(false);
   const navigate = useNavigate();
 
   const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
   const closeSidebar = () => setIsSidebarOpen(false);
 
-  // When Write is clicked
   const handleWriteClick = () => {
     if (!isLogged) {
-      navigate("/accounts"); // Redirect to accounts if not logged in
+      navigate("/accounts");
     } else {
       console.log("Open Write Editor");
     }
@@ -27,7 +23,6 @@ const Header = () => {
   return (
     <>
       <div className="flex justify-between items-center px-5 sm:px-7 md:px-8 py-3 bg-[#222]">
-        {/* Sidebar Toggle for Mobile */}
         <div className="menu shadow-[inset_0_-12px_9px_rgba(0,0,0,0.8)] px-2 py-2 rounded-md lg:hidden">
           <Menu
             onClick={toggleSidebar}
@@ -47,12 +42,11 @@ const Header = () => {
           </div>
         </Link>
 
-        {/* Navbar for Desktop */}
+        
         <div className="hidden lg:inline-block">
           <Navbar />
         </div>
 
-        {/* Right Side: Profile or Write Button */}
         <div className="side2 flex items-center space-x-3">
           {isLogged ? (
             <div className="profile bg-gray-200 rounded-full p-4 md:p-6"></div>
@@ -67,18 +61,16 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Sidebar Overlay (click outside to close) */}
       {isSidebarOpen && (
         <div className="fixed inset-0 z-40" onClick={closeSidebar} />
       )}
 
-      {/* Sidebar */}
       <div
         className={`fixed top-0 left-0 h-full w-full bg-[#222] shadow-lg transform transition-transform duration-300 ease-in-out z-50
                 ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
         onClick={(e) => e.stopPropagation()}
       >
-        {/* Sidebar Header */}
+
         <div className="flex justify-between items-center px-5 py-3">
           <Link to="/">
             <div className="side1 shadow-[inset_0_-12px_9px_rgba(0,0,0,0.8)] px-1 py-1 rounded-full">
