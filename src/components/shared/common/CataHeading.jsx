c6cd1bb4ca56f37ec4580664d2fc2eee4d54aa7e
Abdur Razzaq <devarcoder789@gmail.com>
2025-08-12 10:22:46 +0500

    still continue

--- src/components/shared/common/CataHeading.jsx
+++ src/components/shared/common/CataHeading.jsx
@@ -5,7 +5,7 @@ const CataHeading = ({heading, paragraph}) => {
     <>
       <div className="text-white pt-6 space-y-2">
         <h1 className='text-3xl font-bold'>{heading}</h1>
-        <p className='text-gray-300'>{paragraph}</p>
+        <p className='text-gray-300 w-[70%] md:w-[50%]'>{paragraph}</p>
       </div>
     </>
   )
